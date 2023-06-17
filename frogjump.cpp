8178380e0304cc0dba97ff98d7317899512eb885
abrknine <abhishekbahukhandi2020@gmail.com>
2023-06-17 23:49:27 +0530

    dpin1d

---
+++ frogjump.cpp
@@ -0,0 +1,59 @@
+//same as min cost stair
+//dp in 1D
+#include <bits/stdc++.h>
+#include<vector>
+#include<climits>
+
+
+//MEMOIZATION AORROACH TOP TO BOTTOM->converted from recursion
+/*
+int  f(int ind ,vector<int>& heights, vector<int>& d){
+
+ if(ind==0){
+        return 0;
+    }
+    if(d[ind]!=-1){
+        return d[ind];
+    }
+     int step1=f(ind-1,heights,d)+abs(heights[ind]-heights[ind-1]);
+     int step2=INT_MAX;
+     if(ind>1){
+     int step2=f(ind-2,heights,d)+abs(heights[ind]-heights[ind-2]);
+     }
+    return min(step1,step2);
+}
+int frogJump(int n, vector<int> &heights)
+{
+    vector<int> d(n+1,-1);
+    // Write your code here.
+    return f(n-1,heights,d);
+}
+*/
+//BOTTOM UP
+
+
+int frogJump(int n, vector<int> &heights)
+{
+        vector<int> d(n,0);
+            d[0]=0;
+            //d[1]=abs(heights[1]-heights[0]);
+
+ for(int i=1; i<n; i++){
+    int step1= d[i-1]+abs(heights[i]-heights[i-1]);
+    int step2=INT_MAX;
+    if(i>1){
+      step2=d[i-2]+abs(heights[i]-heights[i-2]);
+    }
+
+
+       d[i]=min(step1,step2);
+
+    }
+
+    return d[n-1];
+
+
+    
+
+    
+}
